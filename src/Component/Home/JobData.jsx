8341dfdd60e7a7b9f35681381d239bca4f6285df
skshawan <iamcoming115@gmail.com>
2023-04-11 13:49:33 +0600

    job Details file is update

--- src/Component/Home/JobData.jsx
+++ src/Component/Home/JobData.jsx
@@ -6,19 +6,19 @@ const JobData = ({ job }) => {
    const { img, name, Working_zone, time_zone, location, title, id, salary } = job;
    return (
       <div className=' text-left mb-4 border'>
-         <div >
+         <div className='p-4' >
             <img className='img' src={img} alt="" />
             <p className='text-3xl font-bold'>{title}</p>
             <p>{name}</p>
             <div className=' flex  text-white '>
-               <p className='  rounded bg-indigo-400' > {Working_zone}</p>
-               <p className='ml-4   rounded bg-indigo-400'> {time_zone}</p>
+               <p className='  rounded bg-indigo-400 p-2' > {Working_zone}</p>
+               <p className='ml-4   rounded bg-indigo-400 p-2'> {time_zone}</p>
             </div>
             <div className='flex justify-between'>
                <p>Location:  <span className='text-3xl font-bold'>{location}</span></p>
                <p>salary:{salary}$</p>
             </div>
-            <button className='border rounded text-white bg-indigo-500'><Link to={`/job/${id}`}>Show Details</Link></button>
+            <button className='border rounded text-white bg-indigo-500 p-2'><Link to={`/job/${id}`}>Show Details</Link></button>
          </div>
       </div>
    );
--- src/Component/JobDetails/JobDetails.jsx
+++ src/Component/JobDetails/JobDetails.jsx
@@ -3,25 +3,52 @@ import { useLoaderData, useParams } from 'react-router-dom';
 import '../../../public/Job.json'
 
 const JobDetails = () => {
-    const {jobId} = useParams()
+    const { jobId } = useParams()
     // const jobString = parseInt(jobId);
     // console.log(typeof jobString)
-    
+
     const [jobs, setJobData] = useState({});
 
     useEffect(() => {
         fetch('./../../../public/Job.json')
-         .then(res => res.json())
-         .then(data => {
-            const jobDetail = data.find(jobD => jobD.id === parseInt(jobId))
-            setJobData(jobDetail)
-         });
+            .then(res => res.json())
+            .then(data => {
+                const jobDetail = data.find(jobD => jobD.id === parseInt(jobId))
+                setJobData(jobDetail)
+            });
     }, []);
-
+   const {Job_Description,phone,email,experience,education,title,salary,location,job_Responsibilities}= jobs;
     return (
-        <div>
-            <h1>sha:{jobs.name} </h1>
-        </div>
+        <section className='mt-8 '>
+            <h1 className='bg-indigo-100 mb-12 text-center text-5xl font-bold p-12'>Job Details</h1>
+            <div className='flex justify-between gap-16 w-12/12'>
+                <div className='text-left w-50%'>
+                    <p className='pb-4'><span className='text-2xl font-bold'>Job-Description:</span><span className='pl-4'>{Job_Description}</span></p>
+                    <p className='pb-4'><span className='text-2xl font-bold'>Job-Responsibility:</span><span className='pl-4'>{job_Responsibilities}</span></p>
+                    <p className='pb-4'><span className='text-2xl font-bold'>Education:</span><span className='pl-4'>{education}</span></p>
+                    <p className='pb-4'><span className='text-2xl font-bold'>Experience:</span><span className='pl-4'>{experience}</span></p>
+                </div>
+
+               <div>
+               <div className=' p-4 mr-8 '>
+                    <div className='p-4 bg-indigo-200 text-left'>
+                        <h1 className='pb-4 text-2xl font-bold'>Job-Details</h1>
+                        <hr  className='border-2'/>
+                        <p className='pb-3 font-bold'>Salary:${salary}K.</p>
+                        <p className='pb-3 font-bold'>Job-Title:{title}</p>
+                    </div>
+                    <div className='p-8 bg-indigo-200 text-left'>
+                        <h1 className='pb-4 font-bold text-2xl'>Contact Information</h1>
+                        <hr  className='border-2'/>
+                        <p><span className='font-bold '>Phone:</span><span className='pl-4'>+880{phone}</span></p>
+                        <p><span className='font-bold'>Email:</span><span className='pl-4'>{email}</span></p>
+                        <p><span className='font-bold'>Address:</span><span className='pl-4' >{location}</span></p>
+                    </div>
+                    <button className='bg-indigo-500 text-white rounded mt-4 py-3 px-24'>Apply Now</button>
+                </div>
+               </div>
+            </div>
+        </section>
     );
 };
 
